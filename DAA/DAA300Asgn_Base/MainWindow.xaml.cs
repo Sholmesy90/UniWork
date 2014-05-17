430bacfe604757a8ce930980fa5ef8522b5c0900
Sam Holmes <Sholmesy90@gmail.com>
2014-05-17 10:27:58 +0800

    Added frequency calc to DAA Assign

--- DAA/DAA300Asgn_Base/MainWindow.xaml.cs
+++ DAA/DAA300Asgn_Base/MainWindow.xaml.cs
@@ -24,6 +24,59 @@ namespace Asgn
         {
             InitializeComponent();
         }
-    
+
+        private void btnCompress_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnFreq_Click(object sender, RoutedEventArgs e)
+        {
+            char[] charArray = txtPlain.Text.ToCharArray();
+            int[] frequencyArray = new int[64];
+            for (int i = 0; i < charArray.Length; i++)
+            {
+               int number = 0;
+
+               char c = charArray[i];
+
+               if ((c >= '0') && (c <= '9'))
+                   number = (int)c + 5;
+               else if ((c >= 'a') && (c <= 'z'))
+                   number = (int)c - 96;
+               else if ((c >= 'A') && (c <= 'Z'))
+                   number = (int)c - 64;
+               else if (c == '\n')
+                   number = 63;
+               else if (c == ' ')
+                   number = 0;
+
+               frequencyArray[number]++;
+            }
+            printFreq(frequencyArray);
+        }
+
+        private void printFreq(int[] array)
+        {
+            String finalString = "";
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] != 0)
+                {
+                    if (i == 0)
+                        finalString += "space : ";
+                    else if ((i >= 1) && (i <= 26))
+                        finalString += (char)(i + 64) + " : ";
+                    else if ((i >= 27) && (i <= 52))
+                        finalString += (char)(i + 96) + " : ";
+                    else if ((i >= 53) && (i <= 62))
+                        finalString += (char)(i - 5) + " : ";
+                    else
+                        finalString += "new line : ";
+                    finalString += array[i] + "\n";
+                }
+            }
+            Console.WriteLine(finalString);
+        }
     }
 }
