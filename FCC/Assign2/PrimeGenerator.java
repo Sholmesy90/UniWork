8f8af5d0b203c99848e72cede3157de5b79fd229
Sam Holmes <Sholmesy90@gmail.com>
2014-05-28 08:17:20 +0800

    Did work on FCC, ~ 75%

---
+++ FCC/Assign2/PrimeGenerator.java
@@ -0,0 +1,87 @@
+import java.util.*;
+import java.io.*;
+import java.math.BigInteger;
+
+public class PrimeGenerator
+{
+	int p1;
+	int p2;
+	public PrimeGenerator()
+	{
+		/// Randomly select 2 primes between 10000 & 100000.
+		System.out.println("Generating Primes...");
+		do 
+		{
+			p1 = generatePrime();
+		}
+		while (!confirmedPrime(p1));
+		System.out.println("Prime #1 : " + p1);
+		do 
+		{
+			p2 = generatePrime();
+		}
+		while (!confirmedPrime(p2));
+		System.out.println("Prime #2 : " + p2);
+	}
+
+	private boolean confirmedPrime(int random)
+	{
+		boolean isTrue = true;
+		for (int i = 0; i < 5; i ++)
+		{
+			int a = 0;
+			while (a % 2 == 0)
+			{
+				a = getRandom(1, random);
+			}
+			if(!checkPrime(random, a))
+			{
+				isTrue = false;
+			}
+		}
+		return isTrue;
+	}
+
+	private int generatePrime()
+	{
+		int random = 0;
+		int a = 0;
+		do
+		{
+			a = 0;
+			while (random % 2 == 0)
+			{
+				random = getRandom(10000, 100000);
+			}
+			while (a % 2 == 0)
+			{
+				a = getRandom(1, random);
+			}
+		} 
+		while (!checkPrime(random, a));
+
+		return random;
+	}
+
+	private boolean checkPrime(int p, int a)
+	{
+		int exponent = (p - 1)/2;
+		BigInteger m = new BigInteger(Integer.toString(p));
+		BigInteger e = new BigInteger(Integer.toString(exponent));
+		BigInteger r = new BigInteger(Integer.toString(a));
+		r = r.modPow(e, m);
+		if ((r.intValue() == 1)||(r.intValue() == p-1))
+			return true;
+		else
+			return false;
+	}
+
+	private int getRandom(int min, int max)
+	{
+		Random rand = new Random(System.nanoTime());
+		return  rand.nextInt(max - min) + min;
+	}
+
+	public int getP1() { return p1; }
+	public int getP2() { return p2; }
+}
